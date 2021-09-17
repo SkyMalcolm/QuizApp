7ecbdbf249208aeb65dfe4d0d1c0bfa9eb9ffd82
SkyMalcolm <sky.lennartsson@iths.se>
2021-09-17 15:32:37 +0200

    Fixed restart option

--- app/src/main/java/com/example/quizapp/FailActivity.kt
+++ app/src/main/java/com/example/quizapp/FailActivity.kt
@@ -1,11 +1,23 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fail)
+
+        lateinit var button: Button
+        button = findViewById(R.id.restartButton)
+
+        button.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            finish()
+            startActivity(intent)
+        }
     }
+
 }
